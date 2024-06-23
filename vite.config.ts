c25df5b32452064050f715db24a7b246ee0dc766
JuanSeZ <juan.zanelli@ing.austral.edu.ar>
2024-06-23 12:21:47 -0300

    fix env variables and add nginx conf

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
   return {
     define: {
-      'process.env.FRONTEND_URL': JSON.stringify(env.FRONTEND_URL),
-      'process.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL),
-      'process.env.AUTH0_DOMAIN': JSON.stringify(env.AUTH0_DOMAIN),
-      'process.env.AUTH0_CLIENT_ID': JSON.stringify(env.AUTH0_CLIENT_ID),
-      'process.env.AUTH0_AUDIENCE': JSON.stringify(env.AUTH0_AUDIENCE),
-      'process.env.AUTH0_SCOPE': JSON.stringify(env.AUTH0_SCOPE),
-      'process.env.MANAGER_URL': JSON.stringify(env.MANAGER_URL),
-      'process.env.PERMISSIONS_URL': JSON.stringify(env.PERMISSIONS_URL),
+      'process.env': {
+        FRONTEND_URL: JSON.stringify(env.FRONTEND_URL),
+        BACKEND_URL: JSON.stringify(env.BACKEND_URL),
+        AUTH0_DOMAIN: JSON.stringify(env.AUTH0_DOMAIN),
+        AUTH0_CLIENT_ID: JSON.stringify(env.AUTH0_CLIENT_ID),
+        AUTH0_AUDIENCE: JSON.stringify(env.AUTH0_AUDIENCE),
+        AUTH0_SCOPE: JSON.stringify(env.AUTH0_SCOPE),
+        MANAGER_URL: JSON.stringify(env.MANAGER_URL),
+        PERMISSIONS_URL: JSON.stringify(env.PERMISSIONS_URL),
+      }
     },
     plugins: [react()],
   }
